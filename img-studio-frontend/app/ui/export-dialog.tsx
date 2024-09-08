5bb026f7e6cc58d91e8b75b89e9c7653963494c1
Agathe Duboué <aduboue@google.com>
2024-09-08 23:11:23 +0200

    :)

--- img-studio-frontend/app/ui/export-dialog.tsx
+++ img-studio-frontend/app/ui/export-dialog.tsx
@@ -66,8 +66,12 @@ export default function ExportStepper({
   const [activeStep, setActiveStep] = React.useState(0)
   const handleNext = () => {
     setActiveStep((prevActiveStep) => prevActiveStep + 1)
+    setIsCloseWithoutSubmit(false)
   }
-  const handleBack = () => setActiveStep((prevActiveStep) => prevActiveStep - 1)
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1)
+    setIsCloseWithoutSubmit(false)
+  }
   const [isCloseWithoutSubmit, setIsCloseWithoutSubmit] = useState(false)
 
   const onCloseTry: DialogProps['onClose'] = (
