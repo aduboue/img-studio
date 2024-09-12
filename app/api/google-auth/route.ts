f786a5f8183d5d4da175704c7e1f1ed338cfdd5d
Agathe Duboué <aduboue@google.com>
2024-09-12 17:06:18 +0200

    Update route.ts

--- app/api/google-auth/route.ts
+++ app/api/google-auth/route.ts
@@ -2,6 +2,8 @@ import { NextRequest, NextResponse } from 'next/server';
 const { GoogleAuth } = require('google-auth-library');
 
 export async function GET(req: NextRequest) {
+  let response = {}
+
   try {
     const auth = new GoogleAuth({
       scopes: 'https://www.googleapis.com/auth/cloud-platform',
@@ -9,17 +11,25 @@ export async function GET(req: NextRequest) {
     const client = await auth.getClient();
 
     let email
-    if (client !== undefined) { email = client.targetPrincipal }
-    else { email = req.headers.get('X-Goog-Authenticated-User-Email') }
+    if (client !== undefined) {
+      response = {
+        targetPrincipal: client.targetPrincipal,
+      }
+    }
+    else { throw Error('No ADC possible') }
 
     console.log("XXXXXXXXXXXX EMAIL " + email) //#TODO temp log
+  } catch (error) {
     console.log("XXXXXXXXXXXX HEADERS " + JSON.stringify(req.headers, undefined, 4)) //#TODO temp log
 
-    return NextResponse.json({
-      targetPrincipal: email,
-    });
-  } catch (error) {
-    console.error(error);
-    return NextResponse.json({ error: 'Authentication error', status: 500 });
+    if (req.headers && req.headers.get('X-Goog-Authenticated-User-Email')) {
+      response = {
+        targetPrincipal: req.headers.get('X-Goog-Authenticated-User-Email'),
+      }
+    } else {
+      console.error(error);
+      response = { error: 'Authentication error', status: 500 }
+    }
   }
+  return NextResponse.json(response);
 }
