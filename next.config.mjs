c3d460f78bbffa3a6986a023ad02e3f04471ce12
Agathe Duboué <aduboue@google.com>
2024-09-30 16:08:33 +0200

    add webpack

--- next.config.mjs
+++ next.config.mjs
@@ -14,7 +14,7 @@ const nextConfig = {
     minimumCacheTTL: 60, // Minimum cache time in seconds (e.g., 1 minute)
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840], // Common device widths
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384], // Sizes for generated images
-  },
+  }
 }
 
-export default nextConfig;
+export default nextConfig
