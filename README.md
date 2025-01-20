fd9bc6c47c6529fc616949d3b9759409e64a6308
Agathe Duboué <aduboue@google.com>
2025-01-20 17:03:15 +0100

    Licence copyright on Googlers' code

--- README.md
+++ README.md
@@ -220,3 +220,7 @@
       `}`
       `}`
     - \> Publish
+
+.
+
+> ###### _This is not an officially supported Google product. This project is not eligible for the [Google Open Source Software Vulnerability Rewards Program](https://bughunters.google.com/open-source-security)._
--- app/api/imagen/action.tsx
+++ app/api/imagen/action.tsx
@@ -1,3 +1,17 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 'use server'
 
 import {
@@ -521,6 +535,7 @@ export async function editImage(formData: EditImageFormI, appContext: appContext
   // 4 - Creating output image list
   try {
     const imagesInGCS: VisionGenerativeModelResultI[] = res.data.predictions
+
     const enhancedImageList = await buildImageList({
       imagesInGCS: imagesInGCS,
       aspectRatio: formData['ratio'],
